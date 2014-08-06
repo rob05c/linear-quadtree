f07d5112cba88adf048bb8f8e6c451d592cb50e7
Robert Butts <robert.o.butts@gmail.com>
2014-08-05 23:24:49 -0600

    added CUDA blocking for arrays larger than the available GPU memory

--- lqt.cu
+++ lqt.cu
@@ -11,10 +11,78 @@
 
 using namespace cub; // debug
 
-
-
 /// \todo fix to not be global
 CachingDeviceAllocator g_allocator(true); // CUB caching allocator for device memory
+
+/// \returns the device totalGlobalMem
+inline size_t GetDeviceMemory() {
+  cudaDeviceProp properties;
+  int deviceNum;
+  CubDebugExit(cudaGetDevice(&deviceNum));
+  CubDebugExit(cudaGetDeviceProperties(&properties, deviceNum));
+  return properties.totalGlobalMem;
+}
+
+#ifndef MIN
+#define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
+#endif
+
+inline size_t find_min(location_t* keys, const size_t keys_len) {
+  if(keys_len == 0)
+    return 0;
+  location_t min = keys[0];
+  size_t min_key = 0;
+  for(size_t i = 0, end = keys_len; i != end; ++i) {
+    if(keys[i] < min) {
+      min_key = i;
+      min = keys[i];
+    }
+  }
+  return min_key;
+}
+
+/// \param[out] keys must be at least block_len large
+/// \return whether all iterators are past their length. That is, when this is false, we can stop merging.
+inline bool get_keys(location_t* keys, const struct linear_quadtree* array_blocks, const size_t block_len, const size_t* iterators) {
+  bool got_key = false;
+  for(int i = 0, end = block_len; i != end; ++i) {
+    if(iterators[i] >= array_blocks[i].length) {
+      keys[i] = location_t_max; // we've iterated past this block's len; make sure this key is never the min.
+      continue;
+    }
+    got_key = true;
+    keys[i] = array_blocks[i].locations[iterators[i]];
+  }
+  return got_key;
+}
+
+struct linear_quadtree lqt_merge(struct linear_quadtree* array_blocks, const size_t block_len, struct lqt_point* points, const size_t len) {
+  struct linear_quadtree lqt;
+  lqt.points    = points;
+  lqt.locations = (location_t*) malloc(sizeof(location_t) * len);
+  lqt.length    = len;
+  if(len == 0)
+    return lqt;
+
+  size_t lqt_iterator = 0;
+  size_t* iterators = (size_t*) malloc(sizeof(size_t) * block_len);
+  for(size_t i = 0, end = block_len; i != end; ++i)
+    iterators[i] = 0;
+
+  {
+    location_t keys[block_len];  
+    for(size_t i = 0; get_keys(keys, array_blocks, block_len, iterators); ++i) {
+      const size_t min_block = find_min(keys, block_len);
+      lqt.locations[lqt_iterator] = array_blocks[min_block].locations[iterators[min_block]];
+      lqt.points[lqt_iterator]    = array_blocks[min_block].points[iterators[min_block]];
+      ++iterators[min_block];
+      ++lqt_iterator;
+    }
+  }
+  
+  free(iterators);
+  return lqt;
+}
 
 __global__ void nodify_kernel(struct lqt_point* points, location_t* locations,
                                  const size_t depth, ord_t xstart, ord_t xend, 
@@ -49,7 +117,34 @@ struct linear_quadtree lqt_create_cuda(struct lqt_point* points, size_t len,
                                        ord_t xstart, ord_t xend, 
                                        ord_t ystart, ord_t yend,
                                        size_t* depth) {
-  return lqt_sortify_cuda_mem(lqt_nodify_cuda_mem(points, len, xstart, xend, ystart, yend, depth));
+  // debug
+  size_t cuda_mem_free = 0;
+  size_t cuda_mem_total = 0;
+  CubDebugExit(cudaMemGetInfo(&cuda_mem_free, &cuda_mem_total));
+
+  const size_t array_size = (sizeof(struct lqt_point) + sizeof(location_t)) * len * 2; // *2 for double-buffers
+  const size_t num_blocks = array_size / cuda_mem_free + 1;
+  printf("num blocks: %lu\n", num_blocks); // debug
+  const size_t array_block_size = array_size / num_blocks;
+  printf("free: %lu\tarray: %lu\tblocks: %lu\tblock size: %lu\n", cuda_mem_free, array_size, num_blocks, array_block_size); // debug
+  
+  const size_t block_len = len / num_blocks + (len % num_blocks != 0 ? 1 : 0);
+  struct linear_quadtree* array_blocks = (struct linear_quadtree*) malloc(num_blocks * sizeof(linear_quadtree));
+
+  for(size_t i = 0, end = num_blocks; i != end; ++i) {
+    array_blocks[i].length = block_len;
+    if(block_len * i + block_len  > len)
+      array_blocks[i].length -= block_len * num_blocks - len; // fix the last block overlap
+    array_blocks[i].points = (struct lqt_point*) malloc(sizeof(struct lqt_point) * array_blocks[i].length);
+    memcpy(array_blocks[i].points, points + block_len * i, array_blocks[i].length * sizeof(struct lqt_point));
+    array_blocks[i] = lqt_sortify_cuda_mem(lqt_nodify_cuda_mem(array_blocks[i].points, array_blocks[i].length, xstart, xend, ystart, yend, depth));
+  }
+  
+  struct linear_quadtree lqt = lqt_merge(array_blocks, num_blocks, points, len);
+  for(size_t i = 0, end = num_blocks; i != end; ++i)
+    lqt_delete(array_blocks[i]);
+  free(array_blocks);
+  return lqt;
 }
 
 /// unnecessarily allocates and frees CUDA memory twice
@@ -189,7 +284,10 @@ struct linear_quadtree_cuda lqt_nodify_cuda_mem(struct lqt_point* points, size_t
   return lqt;
 }
 
+
 struct linear_quadtree lqt_sortify_cuda_mem(struct linear_quadtree_cuda cuda_lqt) {
+  //  printf("DEBUG lqt_sortify_cuda_mem\n"); // debug
+
   DoubleBuffer<location_t> d_keys;
   DoubleBuffer<lqt_point> d_values;
   d_keys.d_buffers[0]   = cuda_lqt.cuda_locations; // reuse the nodify CUDA memory for the cub buffers
@@ -200,6 +298,7 @@ struct linear_quadtree lqt_sortify_cuda_mem(struct linear_quadtree_cuda cuda_lqt
   size_t temp_storage_bytes = 0;
   void* d_temp_storage = NULL;
   CubDebugExit( DeviceRadixSort::SortPairs(d_temp_storage, temp_storage_bytes, d_keys, d_values, cuda_lqt.length));
+  //  printf("temp storage: %lu\n", temp_storage_bytes);  // debug
   CubDebugExit( g_allocator.DeviceAllocate(&d_temp_storage, temp_storage_bytes));
 
   CubDebugExit( DeviceRadixSort::SortPairs(d_temp_storage, temp_storage_bytes, d_keys, d_values, cuda_lqt.length));
